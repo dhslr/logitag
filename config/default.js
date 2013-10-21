15c26ff9b31d41cc553f2f929abbf4e9beb1fa9a
Daniel <tequnix@gmail.com>
2013-10-21 12:05:22 +0200

    deactivated activation mode

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 exports.appool_url = "http://localhost:8079";
 exports.deploy = true;
-exports.activation_mode = true;
+exports.activation_mode = false;
 // values in ms
 exports.window_size = 1000;
 exports.window_epsilon = 200;
